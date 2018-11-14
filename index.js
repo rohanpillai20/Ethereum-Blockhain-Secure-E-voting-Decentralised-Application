46b27344df38973cb7e7c7cf1d4fc2a1e4a00b93
Rohan Pillai <32511124+rohanpillai20@users.noreply.github.com>
2018-11-14 23:53:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,103 +1,66 @@
-
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
-//abi = JSON.parse('[ { constant: true,inputs: [Array],name: \'totalVotesFor\',outputs: [Array],payable: false,stateMutability: \'view\',type: \'function\' },{ constant: true,inputs: [Array],name: \'validCandidate\',outputs: [Array],payable: false,stateMutability: \'view\',type: \'function\' },{ constant: true,inputs: [Array],name: \'votesReceived\',outputs: [Array],payable: false,stateMutability: \'view\',type: \'function\' },{ constant: true,inputs: [Array],name: \'candidateList\',outputs: [Array],payable: false,stateMutability: \'view\',type: \'function\' },{ constant: false,inputs: [Array],name: \'voteForCandidate\',outputs: [],payable: false,stateMutability: \'nonpayable\',type: \'function\' },{ inputs: [Array],payable: false,stateMutability: \'nonpayable\',type: \'constructor\' } ],totalVotesFor:{ [Function: bound ]request: [Function: bound ],call: [Function: bound ],sendTransaction: [Function: bound ],estimateGas: [Function: bound ],getData: [Function: bound ],bytes32: [Circular] },validCandidate:{ [Function: bound ]request: [Function: bound ],call: [Function: bound ],sendTransaction: [Function: bound ],estimateGas: [Function: bound ],getData: [Function: bound ],bytes32: [Circular] },votesReceived:{ [Function: bound ]request: [Function: bound ],call: [Function: bound ],sendTransaction: [Function: bound ],estimateGas: [Function: bound ],getData: [Function: bound ],bytes32: [Circular] },candidateList:{ [Function: bound ]request: [Function: bound ],call: [Function: bound ],sendTransaction: [Function: bound ],estimateGas: [Function: bound ],getData: [Function: bound ],uint256: [Circular] },voteForCandidate:{ [Function: bound ]request: [Function: bound ],call: [Function: bound ],sendTransaction: [Function: bound ],estimateGas: [Function: bound ],getData: [Function: bound ],bytes32: [Circular] },allEvents: [Function: bound ]')
-
-
-abi = JSON.parse('[{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"x","type":"bytes32"}],"name":"bytes32ToString","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"contractOwner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"type":"constructor"}]')
+abi=JSON.parse('[{"constant":false,"inputs":[{"name":"_candidateId","type":"uint256"}],"name":"vote","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"candidatesCount","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidates","outputs":[{"name":"id","type":"uint256"},{"name":"name","type":"string"},{"name":"voteCount","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"voters","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]')
 
 var votedOrNot=[]
 
 VotingContract = web3.eth.contract(abi);
 // In your nodejs console, execute contractInstance.address to get the address at which the contract is deployed and change the line below to use your deployed address
-contractInstance = VotingContract.at('0x0c45546523fd43693e91a83ef60529a2ac2e559f');
-candidates = {"Bill": "candidate-1", "Tom": "candidate-2", "Janice": "candidate-3"}
+var contractInstance = VotingContract.at('0x1079a9cd55d231ee299bb7a7915716bb177d5c6f');
+var candidates = {"Bill": 1, "Tom": 2, "Janice": 3}
 
 function voteForCandidate() {
-  var account = prompt("Please enter your registered number");
-  //var account = 0;
-  if(account in votedOrNot){
-	  alert("You have already voted");
-  }else{
-	  votedOrNot.push(account);
-	  var e = document.getElementById("candidate").value;
-	  candidateName = e;
-	  //candidateName = $("#candidate").val();
-	contractInstance.voteForCandidate(candidateName, {from: web3.eth.accounts[account]}, function() {
-    let div_id = candidates[candidateName];
-    $("#" + div_id).html(contractInstance.totalVotesFor.call(candidateName).toString());
-  });
-  }
-  
- console.log(votedOrNot)
+	var account = prompt("Please enter your registered number");
+	$("#ID").html("Your Account ID: "+web3.eth.accounts[account]);
+	$("#bal").html("Balance: "+web3.eth.getBalance(web3.eth.accounts[account])/Math.pow(10,18));
+	var e = document.getElementById("candidate").value;
+	var candidateName = e;	
+
+	try{
+		contractInstance.vote(candidates[candidateName], {from: web3.eth.accounts[account]}); 
+	}catch(err){
+		if(err.toString()=='Error: VM Exception while processing transaction: revert'){
+			alert("You have already voted.\n" + err);
+		}
+	}finally{
+		$("#bal").html("Balance: "+web3.eth.getBalance(web3.eth.accounts[account])/Math.pow(10,18));
+	}
 }
 
-$(document).ready(function() {
-  candidateNames = Object.keys(candidates);
-  for (var i = 0; i < candidateNames.length; i++) {
-    let name = candidateNames[i];	
-	let val = contractInstance.totalVotesFor.call(name).toString()
-    $("#" + candidates[name]).html(val);
-  }
-});
-/*
+function countForCandidate() {
+	var today = new Date();
+	var dd = today.getDate();
+	var mm = today.getMonth()+1; //January is 0!
+	var yyyy = today.getFullYear();
+	if(dd<10) {
+		dd = '0'+dd
+	} 
+	if(mm<10) {
+		mm = '0'+mm
+	} 
+	today = dd + '/' + mm + '/' + yyyy;
 
-(() => {
-const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-const interface = JSON.parse('[{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"x","type":"bytes32"}],"name":"bytes32ToString","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"contractOwner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"type":"constructor"}]');
-const VotingContract = web3.eth.contract(interface);
-const contractInstance = VotingContract.at('0x2f8ab4c0be9cf788891afc42aca978e970d409a2');
 
-// Default account is used if you don't specify from in function call.
-//web3.eth.defaultAccount = web3.eth.accounts[0];
-web3.personal.unlockAccount(web3.eth.accounts[0], '0x6f59064197812aee1ba001f7d49304c259594b0a1c02e8c2fa2f1d0ad0bf6297', 0);
-
-const tableElem = document.getElementById("table-body");
-const candidateOptions = document.getElementById("candidate-options");
-const voteForm = document.getElementById("vote-form");
-
-function handleVoteForCandidate(evt) {
-  const candidate = new FormData(evt.target).get("candidate");
-  contractInstance.voteForCandidate(candidate, {from: web3.eth.accounts[0]}, function() {
-    const votes = contractInstance.totalVotesFor.call(candidate);
-
-    // Updates the vote element.
-    document.getElementById("vote-" + candidate).innerText = votes;
-  });
+	if(today.toString()=='13/11/2018'){
+		document.getElementById('win1').disabled = false;
+		for(var i=1;i<4;i++){
+			$("#candidate-"+i).html(contractInstance.candidates(i)[2].toString());		
+		}
+	}else{
+		alert("The results will be declared on 13/11/2018");
+	}		
 }
 
-voteForm.addEventListener("submit", handleVoteForCandidate, false);
-
-function populateCandidates() {
-  const candidateList = contractInstance.getCandidateList.call(); // call() is used for sync read only calls.
-  candidateList.forEach((candidate) => {
-    const candidateName = web3.toUtf8(candidate);
-    const votes = contractInstance.totalVotesFor.call(candidate);
-    
-    // Creates a row element.
-    const rowElem = document.createElement("tr");
-
-    // Creates a cell element for the name.
-    const nameCell = document.createElement("td");
-    nameCell.innerText = candidateName;
-    rowElem.appendChild(nameCell);
-
-    // Creates a cell element for the votes.
-    const voteCell = document.createElement("td");
-    voteCell.id = "vote-" + candidate; 
-    voteCell.innerText = votes;
-    rowElem.appendChild(voteCell);
-
-    // Adds the new row to the voting table.
-    tableElem.appendChild(rowElem);
-
-    // Creates an option for each candidate
-    const candidateOption = document.createElement("option");
-    candidateOption.value = candidate;
-    candidateOption.innerText = candidateName;
-    candidateOptions.appendChild(candidateOption);
-  });
+function winner(){
+	var max = 0;
+	var candidateW = '';
+	for(var i=1;i<4;i++){
+		var votes = contractInstance.candidates(i)[2].toString();		
+		if(max<votes){
+			candidateW = contractInstance.candidates(i)[1].toString();
+			max = votes
+		}
+	}
+	//alert(max + " "+ candidateW);
+	$("#win").html("Winner is: " + candidateW);
 }
-
-populateCandidates();
-})();*/
